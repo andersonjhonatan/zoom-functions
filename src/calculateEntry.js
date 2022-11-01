588d0cd76339ed2339f3c8012ee4fb412d2e2d24
Anderson Jhonatan <ajhonatan76@gmail.com>
2022-11-01 14:34:17 -0300

    8 - Requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu códdddigo aqui
+  const nomes = {};
+  const child = entrants.filter(({ age }) => age < 18);
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50);
+  const senior = entrants.filter(({ age }) => age >= 50);
+  nomes.child = child.length;
+  nomes.adult = adult.length;
+  nomes.senior = senior.length;
+  return nomes;
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const pricesChild = countEntrants(entrants).child * data.prices.child;
+  const pricesAdults = countEntrants(entrants).adult * data.prices.adult;
+  const priceSenior = countEntrants(entrants).senior * data.prices.senior;
+  const total = pricesChild + priceSenior + pricesAdults;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
