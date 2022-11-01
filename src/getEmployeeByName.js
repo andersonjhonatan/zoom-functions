dfdd4531aae5083e3f3098fcf697611940c92853
Anderson Jhonatan <ajhonatan76@gmail.com>
2022-10-31 21:16:51 -0300

    3 - Requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees.find((elemento) => elemento
+    .firstName === employeeName || elemento.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
