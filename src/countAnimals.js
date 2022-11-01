fcae83705c83d4d30ba6613c0e7448a793b5e2fc
Anderson Jhonatan <ajhonatan76@gmail.com>
2022-11-01 11:18:48 -0300

    5 - Requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  if (animal === undefined) {
+    const novoObjeto = {};
+    species.forEach((especie) => {
+      novoObjeto[especie.name] = especie.residents.length;
+    });
+    return novoObjeto;
+  }
+
+  if (Object.keys(animal).length === 1) {
+    return species.find((specie) => specie.name === animal.specie).residents
+      .length;
+  }
+
+  return species
+    .find((specie) => specie.name === animal.specie)
+    .residents.filter((elemento) => elemento.sex === animal.sex).length;
+}
 module.exports = countAnimals;
