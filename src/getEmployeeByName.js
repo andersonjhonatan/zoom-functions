d546f94022be9a62596db1b1d6ab98af627e4dee
Anderson Jhonatan <ajhonatan76@gmail.com>
2022-11-02 00:13:59 -0300

    9 - Requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -4,10 +4,8 @@ const { employees } = data;
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) return {};
-  return employees.find(
-    ({ lastName, firstName }) =>
-      firstName === employeeName || lastName === employeeName
-  );
+  return employees.find(({ lastName, firstName }) =>
+    firstName === employeeName || lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
