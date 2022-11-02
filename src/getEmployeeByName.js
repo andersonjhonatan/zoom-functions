84fffad7d5cf7b34a546d59874174d1d2ba9ecab
Anderson Jhonatan <ajhonatan76@gmail.com>
2022-11-01 23:56:48 -0300

    9 - Requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,9 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) return {};
-  return data.employees.find((elemento) => elemento
-    .firstName === employeeName || elemento.lastName === employeeName);
+  return employees.find(
+    ({ lastName, firstName }) =>
+      firstName === employeeName || lastName === employeeName
+  );
 }
 
 module.exports = getEmployeeByName;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,73 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { species, hours } = data;
+
+const getElemento = (elementos) => {
+  const getAnimal = species.filter(({ availability }) =>
+    availability.includes(elementos));
+  const getHours = hours[elementos];
+  const getObjeto = {
+    [elementos]: {
+      officeHour: `Open from ${getHours.open}am until ${getHours.close}pm`,
+      exhibition: getAnimal.map(({ name }) => name),
+    },
+  };
+  return getObjeto;
+};
+
+const getElemento2 = (day) => {
+  const days = hours[day];
+  return `Open from ${days.open}am until ${days.close}pm`;
+};
+
+const generateAnimals = (generate) => {
+  const animalValidacao = species.filter((id) =>
+    id.availability.includes(generate));
+  return animalValidacao.map(({ name }) => name);
+};
+
+const mondays = {
+  officeHour: 'CLOSED',
+  exhibition: 'The zoo will be closed!',
+};
+
+const zoolo = () => {
+  const myDay = {
+    Tuesday: null,
+    Wednesday: null,
+    Thursday: null,
+    Friday: null,
+    Saturday: null,
+    Sunday: null,
+    Monday: mondays,
+  };
+  species.forEach(({ availability }) => {
+    availability.forEach((day) => {
+      myDay[day] = {
+        officeHour: getElemento2(day),
+        exhibition: generateAnimals(day),
+      };
+    });
+  });
+  return myDay;
+};
+
+function dayCode(days) {
+  const day = Object.keys(hours);
+  return day.some((key) => key !== 'Monday' && key === days);
+}
+
+function getSchedule(target) {
+  if (target === 'Monday') {
+    return { Monday: mondays };
+  }
+  if (dayCode(target)) {
+    return getElemento(target);
+  }
+  if (species.some(({ name }) => name === target)) {
+    return species.find(({ name }) => name === target).availability;
+  }
+  return zoolo();
 }
 
 module.exports = getSchedule;
